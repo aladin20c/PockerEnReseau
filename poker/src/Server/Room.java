9ff14c548aa9309b445cd106dcd0e4d94b648876
cheniour <aladincheniour19@gmail.com>
2022-02-04 19:43:33 +0100

    fixes

--- poker/src/Server/Room.java
+++ poker/src/Server/Room.java
@@ -3,11 +3,9 @@ package Server;
 import java.util.ArrayList;
 
 public class Room {
-
-    private ArrayList<ClientHandler> clientHandlers;
-
+    
     public Room(int type, int numberOfPlayers,int bet,int stack) {
-        this.clientHandlers = new ArrayList<>();
+
     }
 
 }
