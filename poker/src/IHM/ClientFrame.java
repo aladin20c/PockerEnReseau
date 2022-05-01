9d27c3a07ea6e70f5b97c91a03b57314d0f861ce
djeghali <nadinedjeghali@hotmail.com>
2022-05-01 13:23:24 +0200

    showing cards on table

--- poker/src/IHM/ClientFrame.java
+++ poker/src/IHM/ClientFrame.java
@@ -23,7 +23,7 @@ public class ClientFrame extends JFrame {
     private  JTextField     potText;                        // How much money is in the pot
     private  JTextField     betText;                        // How much is the current bet
     private  JTextField     yourBetText;                    // How much is this player about to bet
-    private ArrayList       positionLabels;                 // Labels under each possible player location
+    private  ArrayList      positionLabels;                 // Labels under each possible player location
     private  ArrayList      cashLabels;                     // Labels showing how much money each player has.
     private  ArrayList      dealerLabels;                   // Labels with icon of dealer button to designate who is the dealer.
     private  String         playerText[];                   // String of the text under each player
@@ -40,6 +40,7 @@ public class ClientFrame extends JFrame {
     private ArrayList<JComponent> createPanel = new ArrayList<JComponent>() ;
     private ArrayList<JLabel> cardsLabels = new ArrayList<JLabel>();
     private ArrayList<JLabel> namesLabels = new ArrayList<JLabel>();
+    private ArrayList<JLabel> cardsOnTable = new ArrayList<JLabel>();
 
 
 
@@ -53,6 +54,7 @@ public class ClientFrame extends JFrame {
         super(title);
         this.client=client;
         playerText = new String[MAX_PLAYERS];
+
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout( new GridLayout( 3,1 ) );
         JPanel buttons = new JPanel();
@@ -78,23 +80,27 @@ public class ClientFrame extends JFrame {
         cashPanel = new JPanel();
         cashPanel.setLayout( new GridLayout( 1,4 ) );
         cashPanel.setBorder( new EtchedBorder( EtchedBorder.RAISED ) );
-        bankText = new JTextField();
-        potText = new JTextField();
-        betText = new JTextField();
+
+        bankText = new JTextField("Bank ");
+        potText = new JTextField("Pot ");
+        betText = new JTextField("Bet");
+
         bankText.setEditable( false );
         potText.setEditable( false );
         betText.setEditable( false );
+
         cashPanel.add( bankText );
         cashPanel.add( potText );
         cashPanel.add( betText );
 
         messagePanel = new JPanel();
         messagePanel.setLayout( new BorderLayout() );
-        messageText = new JTextField();
+        messageText = new JTextField("Send a message");
         messageText.setEditable( false );
         messagePanel.add( messageText );
-        yourBetText = new JTextField();
-        yourBetText.setPreferredSize( new Dimension( 130,1 ) );
+
+        yourBetText = new JTextField("Your bet");
+        yourBetText.setPreferredSize( new Dimension( 325,1 ) );
         yourBetText.setEditable( false );
         messagePanel.add( yourBetText, BorderLayout.EAST );
 
@@ -106,6 +112,7 @@ public class ClientFrame extends JFrame {
 
         table = new JPanel( null);
         table.setBackground(new Color(0, 128, 0));
+
         startGame = new JButton(new AbstractAction() {
             public void actionPerformed(ActionEvent a) {
                 setPanel(joinPanel);
@@ -114,9 +121,11 @@ public class ClientFrame extends JFrame {
         startGame.setText("Start the game");
         startGame.setBounds(610,230,150,30);
         startGamePanel.add(startGame);
+
         join = new JTextField();
         join.setBounds(610,230,150,30);
         joinPanel.add(join);
+
         okName = new JButton(new AbstractAction() {
             public void actionPerformed(ActionEvent a) {
                 String messageToSend="100 HELLO PLAYER "+join.getText();
@@ -170,19 +179,25 @@ public class ClientFrame extends JFrame {
         createRound.setText("Créer la partie");
         createRound.setBounds(400,150,150,30);
         createPanel.add(createRound);
+
+        JLabel dealer=new JLabel();
+        dealer.setIcon(new ImageIcon(this.getClass().getResource("/images/button_present.png")));
+        dealer.setBounds(170,342,50,50);
+        roundPanel.add(dealer);
+
         for (int i=0;i<MAX_PLAYERS;i++) {
             String path;
             if(i==0){
                 JLabel name = new JLabel();
-                name.setBounds(75, 190, 90, 30);
+                name.setBounds(75, 220, 90, 30);
                 namesLabels.add(name);
                 roundPanel.add(name);
                 JLabel[] cards = new JLabel[5];
                 for (int j = 0; j < 5; j++) {
                     cards[j] = new JLabel();
-                    /*path = "/images2/card_44.png";
-                    cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));*/
-                    cards[j].setBounds(50 + (j * 15), 225, 70, 100);
+                    path = "/images/card_44.png";
+                    cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));
+                    cards[j].setBounds(50 + (j * 15), 255, 70, 100);
                     cardsLabels.add(cards[j]);
                 }
                 for (int j = 4; j >= 0; j--) {
@@ -198,8 +213,8 @@ public class ClientFrame extends JFrame {
                     JLabel[] cards = new JLabel[5];
                     for (int j = 0; j < 5; j++) {
                         cards[j] = new JLabel();
-                        /*path = "/images2/card_44.png";
-                        cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));*/
+                        path = "/images/card_back.png";
+                        cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));
                         cards[j].setBounds(80 + (j * 15) + (i*200), 55, 70, 100);
                         cardsLabels.add(cards[j]);
                     }
@@ -210,14 +225,14 @@ public class ClientFrame extends JFrame {
                 else{
                     if(i==5){
                         JLabel name = new JLabel();
-                        name.setBounds(1120, 190, 90, 30);
+                        name.setBounds(1120, 220, 90, 30);
                         roundPanel.add(name);
                         JLabel[] cards = new JLabel[5];
                         for (int j = 0; j < 5; j++) {
                             cards[j] = new JLabel();
-                            /*path = "/images2/card_44.png";
-                            cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));*/
-                            cards[j].setBounds(1100 + (j * 15), 225, 70, 100);
+                            path = "/images/card_back.png";
+                            cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));
+                            cards[j].setBounds(1100 + (j * 15), 255, 70, 100);
                             cardsLabels.add(cards[j]);
                         }
                         for (int j = 4; j >= 0; j--) {
@@ -232,8 +247,8 @@ public class ClientFrame extends JFrame {
                         JLabel[] cards = new JLabel[5];
                         for (int j = 0; j < 5; j++) {
                             cards[j] = new JLabel();
-                            /*path = "/images2/card_44.png";
-                            cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));*/
+                            path = "/images/card_back.png";
+                            cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));
                             cards[j].setBounds(885 +(j * 15) -((i-6)*200), 450, 70, 100);
                             cardsLabels.add(cards[j]);
                         }
@@ -243,11 +258,21 @@ public class ClientFrame extends JFrame {
                     }
                 }
             }
-
+        }
+        JLabel[] cards = new JLabel[5];
+        for (int j = 0; j < 5; j++) {
+            cards[j] = new JLabel();
+            String path = "/images/card_placeholder.png";
+            cards[j].setIcon(new ImageIcon(this.getClass().getResource(path)));
+            cards[j].setBounds(310 + (j * 150) , 255, 75, 100);
+            cardsOnTable.add(cards[j]);
+        }
+        for (int j = 4; j >= 0; j--) {
+            roundPanel.add(cards[j]);
         }
 
         getContentPane().add( table, BorderLayout.CENTER );
-        setPanel(startGamePanel);
+        setPanel(roundPanel);
         setResizable( false );
         setBounds( 35,20,1300,700);
         setDefaultCloseOperation( javax.swing.WindowConstants.DISPOSE_ON_CLOSE );
@@ -267,32 +292,40 @@ public class ClientFrame extends JFrame {
         id.setEditable(false);
         id.setBounds(0,0,150,30);
         getListPanel.add(id);
+
         JTextField type = new JTextField("Type");
         type.setEditable(false);
         type.setBounds(150,0,150,30);
         getListPanel.add(type);
+
         JTextField nbPlayer = new JTextField("Nombre de joueurs");
         nbPlayer.setEditable(false);
         nbPlayer.setBounds(2*150,0,150,30);
         getListPanel.add(nbPlayer);
+
         JTextField minBet = new JTextField("Min bet");
         minBet.setEditable(false);
         minBet.setBounds(3*150,0,150,30);
         getListPanel.add(minBet);
+
         JTextField stack = new JTextField("stack");
         stack.setEditable(false);
         stack.setBounds(4*150,0,150,30);
         getListPanel.add(stack);
+
         String messageToSend="120 GETLIST";
         messageToSend=messageToSend.trim();
         client.analyseMessageToSend(messageToSend);
         client.writeToServer(messageToSend);
+
         setPanel(getListPanel);
         int nbRooms = ((MenuState)client.getGameState()).getN();
+        /*
         while(nbRooms!=0 &&(((MenuState)client.getGameState()).getGamesList()==null || ((MenuState)client.getGameState()).getGamesList(nbRooms-1)==null)){
             System.out.println(nbRooms!=0 &&(((MenuState)client.getGameState()).getGamesList()==null || ((MenuState)client.getGameState()).getGamesList(nbRooms-1)==null));
 
         }
+        */
         for(int i=0 ; i<nbRooms ; i++){
             JTextField id1 = new JTextField();
             id1.setText(""+((MenuState)client.getGameState()).getGamesList(i).getId());
@@ -300,30 +333,35 @@ public class ClientFrame extends JFrame {
             id1.setEditable(false);
             id1.setBackground(Color.WHITE);
             getListPanel.add(id1);
+
             JTextField type1 = new JTextField();
             type1.setText(""+((MenuState)client.getGameState()).getGamesList(i).getType());
             type1.setBounds(150,30*(i+1),150,30);
             type1.setEditable(false);
             type1.setBackground(Color.WHITE);
             getListPanel.add(type1);
+
             JTextField nbPlayer1 = new JTextField();
             nbPlayer1.setText(""+((MenuState)client.getGameState()).getGamesList(i).getPlayers().size());
             nbPlayer1.setBounds(2*150,30*(i+1),150,30);
             nbPlayer1.setEditable(false);
             nbPlayer1.setBackground(Color.WHITE);
             getListPanel.add(nbPlayer1);
+
             JTextField minBet1 = new JTextField();
             minBet1.setText(""+((MenuState)client.getGameState()).getGamesList(i).getMinBid());
             minBet1.setBounds(3*150,30*(i+1),150,30);
             minBet1.setEditable(false);
             minBet1.setBackground(Color.WHITE);
             getListPanel.add(minBet1);
+
             JTextField stack1 = new JTextField();
             stack1.setText(""+((MenuState)client.getGameState()).getGamesList(i).getInitStack());
             stack1.setBounds(4*150,30*(i+1),150,30);
             stack1.setEditable(false);
             stack1.setBackground(Color.WHITE);
             getListPanel.add(stack1);
+
             int index = i;
             JButton joinRoom = new JButton(new AbstractAction() {
                 public void actionPerformed(ActionEvent a) {
@@ -338,6 +376,7 @@ public class ClientFrame extends JFrame {
             joinRoom.setBounds(5*150+30,30*(i+1),90,30);
             getListPanel.add(joinRoom);
         }
+
         JButton create = new JButton(new AbstractAction() {
             public void actionPerformed(ActionEvent a) {
                 setPanel(createPanel);
@@ -346,6 +385,7 @@ public class ClientFrame extends JFrame {
         create.setText("Créer une partie");
         create.setBounds(900,50,150,30);
         getListPanel.add(create);
+
         JButton refresh = new JButton(new AbstractAction() {
             public void actionPerformed(ActionEvent a) {
                 getList();
